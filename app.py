008c715e6f9bd408cfa5b0a5b919a43376320d0f
prashant-19 <prashant.my10@gmail.com>
2024-09-18 00:18:17 +0530

    Save as Zip Avilable

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import instaloader
 import time
+import shutil
+import os
 from instaloader.exceptions import TooManyRequestsException, ProfileNotExistsException
 import socket
 
@@ -14,11 +16,10 @@ def download_profile(loader, username):
         return False
     except ProfileNotExistsException:
         print(f"Profile {username} does not exist.")
-        return True  
+        return True
     except (socket.timeout, TimeoutError):
         print("Timeout occurred, sleeping for 5 hours.")
         return False  
-    
     except Exception as e:
         error_message = str(e)
         if "Please wait a few minutes before you try again" in error_message:
@@ -28,9 +29,20 @@ def download_profile(loader, username):
             print(f"Error downloading profile {username}: {error_message}")
             return True 
 
+def zip_profile(username):
+    profile_dir = f"./{username}"  
+    zip_filename = f"./{username}.zip"  
+    
+    if os.path.isdir(profile_dir): 
+        print(f"Zipping profile: {username}")
+        shutil.make_archive(f"./{username}", 'zip', profile_dir)  
+        print(f"Profile {username} successfully zipped as {zip_filename}")
+    else:
+        print(f"Directory {profile_dir} not found, unable to zip profile.")
+
 def download_profiles_with_rate_limiting(usernames):
     loader = instaloader.Instaloader()
-    
+
     for username in usernames:
         success = False
         while not success:
@@ -38,12 +50,13 @@ def download_profiles_with_rate_limiting(usernames):
             if not success:
                 print(f"Sleeping for 5 hours before retrying profile: {username}")
                 for i in range(5 * 60 * 60):
-                    print(f"Sleeping... {i+1} seconds", end='\r') 
-                    time.sleep(1) 
+                    print(f"Sleeping... {i+1} seconds", end='\r')  
+                    time.sleep(1)  
+        
         print(f"Completed downloading profile: {username}")
+        zip_profile(username) 
 
 if __name__ == "__main__":
-
     usernames = ["appliviu"]
 
     download_profiles_with_rate_limiting(usernames)
